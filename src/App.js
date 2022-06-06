141be8f4a6df21f014d385ee1a883fb2413a49eb
Shubh-Pawar007 <shubhampawar242767@gmail.com>
2022-06-06 17:25:59 +0530

    update1

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
+        <Route path='/' element={<Login/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
       </Routes>
       
